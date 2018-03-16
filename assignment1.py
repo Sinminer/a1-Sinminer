83a35f8a3e328a10eaa8c189be8cfd5fe38ecbf8
Robert Sinclair <32863413+Sinminer@users.noreply.github.com>
2018-03-16 16:42:54 +1000

    Created function load_list which is used to print the amount of movies that have been loaded from the file.

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,16 @@ Replace the contents of this module docstring with your own details.
 
 
 def main():
-    print("Movies To Watch 1.0 - by <Your Name>")
+    print("Movies To Watch 1.0 - by Robert Sinclair.")
+    load_list()
+
+def load_list():
+    movie_amount = len(open("movies.csv").readlines())
+    print("{} Movies loaded".format(movie_amount))
+
+
+
+
 
 
 if __name__ == '__main__':
