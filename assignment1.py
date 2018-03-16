5667f3d79463310e61d3ca8b45fb8b428b043a82
Robert Sinclair <32863413+Sinminer@users.noreply.github.com>
2018-03-16 17:49:54 +1000

    Added the menu function which contains a while loop that looks the Menu operation.

--- assignment1.py
+++ assignment1.py
@@ -6,10 +6,39 @@ Replace the contents of this module docstring with your own details.
 def main():
     print("Movies To Watch 1.0 - by Robert Sinclair.")
     load_list()
+    menu()
+
 
 def load_list():
     movie_amount = len(open("movies.csv").readlines())
     print("{} Movies loaded".format(movie_amount))
+
+
+def menu():
+        choice = ""
+        print("Menu:")
+        print("L - List movies")
+        print("A - Add new movie")
+        print("W - Watch a a movie")
+        print("Q - Quit")
+        while choice not in ["Q","A","L","W"]:
+            choice = (input(">>>")).upper()
+            if choice not in ["Q","A","L","W"]:
+                print("Invalid menu choice")
+                print("Menu:")
+                print("L - List movies")
+                print("A - Add new movie")
+                print("W - Watch a a movie")
+                print("Q - Quit")
+            else:
+                pass
+
+
+
+
+
+
+
 
 
 
