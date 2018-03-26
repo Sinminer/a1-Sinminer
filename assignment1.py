3de06f08a7fb70629c4dde61d6ec87758b140ffb
Robert Sinclair <32863413+Sinminer@users.noreply.github.com>
2018-03-26 19:14:30 +1000

    Added the menu function which contains a while loop that looks the Menu operation.

--- assignment1.py
+++ assignment1.py
@@ -5,13 +5,25 @@ Replace the contents of this module docstring with your own details.
 
 def main():
     print("Movies To Watch 1.0 - by Robert Sinclair.")
-    load_list()
+    movie_list = load_file()
     menu()
 
 
-def load_list():
-    movie_amount = len(open("movies.csv").readlines())
-    print("{} Movies loaded".format(movie_amount))
+def load_file():
+    file = open("movies.csv", "r")  # opens file with name of "test.txt"
+    movie_list = []
+    for line in file:
+        line = (line.strip("\n")).split(",")
+        line[1] = int(line[1])
+        print(str(line))
+        movie_list.append(line)
+    return movie_list
+
+
+
+
+
+
 
 
 def menu():
@@ -19,31 +31,46 @@ def menu():
         print("Menu:")
         print("L - List movies")
         print("A - Add new movie")
-        print("W - Watch a a movie")
+        print("W - Watch a movie")
         print("Q - Quit")
-        while choice not in ["Q","A","L","W"]:
+        while choice not in ["Q"]:
             choice = (input(">>>")).upper()
             if choice not in ["Q","A","L","W"]:
                 print("Invalid menu choice")
                 print("Menu:")
                 print("L - List movies")
                 print("A - Add new movie")
-                print("W - Watch a a movie")
+                print("W - Watch a movie")
                 print("Q - Quit")
-            else:
+            elif choice == "Q":
                 pass
+            elif choice == "A":
+                add_movie()
+            elif choice == "W":
+                watch_movie()
+            else:
+                list_movies()
+
+
+
+def add_movie():
+    def watch_movie():
+        add_watched = open("movies.csv", "r+")
+        add_watched.write("*")
 
 
 
 
 
 
+def list_movies():
+    count = -1
+    movies_list = open("movies.csv", "r+")
+    for line in movies_list:
+        stripped_line = line.rstrip()
+        count += 1
+        print(str(count)+" " + stripped_line)
+    movies_list.close()
+    menu()
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+main()
